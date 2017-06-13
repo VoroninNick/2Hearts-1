2444117f8a963684b3ccafc1487802d966c04e69
pasha <p.korenev@voroninstudio.eu>
2017-06-13 13:04:13 +0300

    app

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,7 +3,7 @@ class ProjectsController < ApplicationController
 
   def index
     @projects = Project.published
-    @categories = ProjectCategory.joins(projects: {}).where(projects: {published: 't'})
+    @categories = ProjectCategory.joins(projects: {}).where(projects: {published: 't'}).uniq
     @other_projects_count = Project.published.where("project_category_id is null").count
     @show_other_category = @other_projects_count > 0
     @all_count = @projects.count
