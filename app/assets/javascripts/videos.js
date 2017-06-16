76c295ebacf56c85906c74bb13f186f381615d62
Bobolay <bogdan4uk@gmail.com>
2017-06-16 18:44:42 +0300

    bg video added

--- app/assets/javascripts/videos.js
+++ app/assets/javascripts/videos.js
@@ -1,18 +1,20 @@
+var video_id = $('#bg-video').attr("data-id")
+
 function onYouTubeIframeAPIReady() {
   var player;
-  player = new YT.Player('muteYouTubeVideoPlayer', {
-    videoId: 'h1o7NGJz1xs', // YouTube Video ID
+  player = new YT.Player('bg-video', {
+    videoId: video_id, // YouTube Video ID
     width: 560,               // Player width (in px)
     height: 316,              // Player height (in px)
     playerVars: {
       autoplay: 1,        // Auto-play the video on load
-      controls: 1,        // Show pause/play buttons in player
+      controls: 0,        // Show pause/play buttons in player
       showinfo: 0,        // Hide the video title
-      modestbranding: 1,  // Hide the Youtube Logo
+      modestbranding: 0,  // Hide the Youtube Logo
       loop: 1,            // Run the video in a loop
       fs: 0,              // Hide the full screen button
       cc_load_policy: 0, // Hide closed captions
-      iv_load_policy: 3,  // Hide the Video Annotations
+      iv_load_policy: 0,  // Hide the Video Annotations
       autohide: 0         // Hide video controls when playing
     },
     events: {
@@ -22,3 +24,4 @@ function onYouTubeIframeAPIReady() {
     }
   });
 }
+onYouTubeIframeAPIReady()
