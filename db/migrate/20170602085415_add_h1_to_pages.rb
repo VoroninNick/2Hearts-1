0090113e11135f94f2f8cd1c585be72e95fffcf1
pkorenev <p.korenev@voroninstudio.eu>
2017-10-24 15:45:24 +0300

    app

--- db/migrate/20170602085415_add_h1_to_pages.rb
+++ db/migrate/20170602085415_add_h1_to_pages.rb
@@ -1,6 +1,11 @@
 class AddH1ToPages < ActiveRecord::Migration
-  def change
-    add_column :pages, :h1_text, :string
-    add_column :page_translations, :h1_text, :string
+  def up
+    add_column :pages, :h1_text, :string if !column_exists?(:pages, :h1_text)
+    add_column :page_translations, :h1_text, :string if !column_exists?(:page_translations, :h1_text)
+  end
+
+  def down
+    remove_column :pages, :h1_text
+    remove_column :page_translations, :h1_text
   end
 end
